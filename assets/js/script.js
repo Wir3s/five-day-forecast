a45e643d7a444e731b17fd4de8895494bba7d773
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-11-27 10:35:39 -0600

    Working on call to api to determine which city is desired

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 var APIKey = "8ab624b5be3ff94ca14f406f1044e429";
 var city;
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+
+var searchButton = document.getElementById("cityButton");
 
 // fetch(queryURL)
 
-var searchButton = document.getElementById("cityButton");
-searchButton.addEventListener("click", function () {});
+searchButton.addEventListener("click", function () {
+  var searchInput = document.getElementById("requestedCity").value;
+  city = searchInput;
+  console.log(city);
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&limit=5&appid=" +
+    APIKey;
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("Weather: Raw data \n----------");
+      console.log(data);
+      console.log(data.current.feelslike_f);
+
+      // var feelsLike = $("</p>").text(
+      //   "Right now it feels like " + data.current.feelslike_f + " degrees outside"
+      // );
+      // mainArea.append(feelsLike);
+    });
+});
