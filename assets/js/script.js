ade4e8e2c0f40b35352a82c0959307e0f42bd9c7
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-11-29 17:43:48 -0600

    Re-working script for smaller, sleeker functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,73 @@
 var APIKey = "8ab624b5be3ff94ca14f406f1044e429";
-var city;
-var addLon;
-var addLat;
-var forecastURL =
-  "http://api.openweathermap.org/data/2.5/forecast?lat=" +
-  addLat +
-  "&lon=" +
-  addLon +
-  "&appid=" +
-  APIKey;
+
 var searchButton = document.getElementById("cityButton");
 
 // fetch(queryURL)
 
 searchButton.addEventListener("click", function () {
   var searchInput = document.getElementById("requestedCity").value;
-  city = searchInput;
-  console.log(city);
+  console.log(searchInput);
+  lookForCity(searchInput);
+});
+
+var lookForCity = function (city) {
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&limit=5&appid=" +
     APIKey;
-
+  console.log(queryURL);
   fetch(queryURL)
     .then(function (response) {
-      return response.json();
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          getFiveDay(data.coord.lat, data.coord.lon);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
     })
-    .then(function (data) {
-      console.log(data);
-      console.log(data.coord.lon, data.coord.lat);
-
-      addLon = data.coord.lon;
-      addLat = data.coord.lat;
-      console.log(addLon, addLat);
-      console.log(JSON.stringify({ location: [city, addLon, addLat] }));
-      localStorage.setItem("location", JSON.stringify(city, addLon, addLat));
+    .catch(function (error) {
+      alert("Unable to obtain location");
     });
-});
+};
+
+var getFiveDay = function (lat, lon) {
+  console.log(lat, lon);
+  var forecastURL =
+    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
+  console.log(forecastURL);
+  fetch(forecastURL).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+};
+
+// .then(function (data) {
+//
+
+//
+//       console.log(addLon, addLat);
+//       console.log(forecastURL);
+//       var cities = document.getElementById("cityList");
+//       var newLink = document.createElement("p");
+//       newLink.innerHTML += "<a " + forecastURL + ">" + city + "</a>";
+//       cities.appendChild(newLink);
+
+//       localStorage.setItem("coords", forecastURL);
+//     });
+// });
 
 // fetch(forecastURL)
 //     .then(function (response) {
